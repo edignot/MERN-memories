1e6fd97b62918a56a74dc5a772e1c1d8b79446be
Ed Ignot <edignot@gmail.com>
2020-11-11 09:33:04 -0700

    Post new form

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,3 +5,7 @@ const url = 'http://localhost:8000/posts'
 export const fetchPosts = async () => {
   return await axios.get(url)
 }
+
+export const createPost = async (post) => {
+  return await axios.post(url, post)
+}
