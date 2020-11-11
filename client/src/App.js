69814a8a1cebb9a7b653f12c81fcd9518c893054
Ed Ignot <edignot@gmail.com>
2020-11-11 11:16:12 -0700

    Post UI display

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
-import memories from './images/memory.jpeg'
 import Form from './components/Form/Form'
 import Posts from './components/Posts/Posts'
 import useStyles from './styles'
@@ -21,12 +20,6 @@ const App = () => {
         <Typography className={classes.heading} variant='h2' align='center'>
           Memories
         </Typography>
-        <img
-          className={classes.image}
-          src={memories}
-          alt='memories'
-          height='60'
-        />
       </AppBar>
       <Grow in>
         <Container>
